31047178ffcf7b79837d224f0bf5fdf81007ae0a
fay-lab-shenzhen <65667743+fay-lab-shenzhen@users.noreply.github.com>
2022-10-18 19:47:44 +0800

    去掉网速测试包

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,3 @@
 # Add a feed source
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git netspeedtest  https://github.com/sirpdboy/netspeedtest' feeds.conf.default
