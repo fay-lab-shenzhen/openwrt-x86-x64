962c85c75dc988ddcfba10878d61d7844707f6f8
fay-lab-shenzhen <65667743+fay-lab-shenzhen@users.noreply.github.com>
2023-03-26 19:00:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.111.1/g' package/base-files/files/bin/config_generate
+
+# Modify default app-store
+./scripts/feeds install -d y -p istore luci-app-store
