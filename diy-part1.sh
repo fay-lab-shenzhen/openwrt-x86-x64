6decb1efbd3f9b0ad02634845ea5a18571d06e0e
fay-lab-shenzhen <65667743+fay-lab-shenzhen@users.noreply.github.com>
2023-09-10 15:07:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,4 @@
 # Add a feed source
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git https://github.com/sirpdboy/luci-theme-kucat.git' feeds.conf.default
-sed -i '$a src-git https://github.com/jerrykuku/luci-theme-argon.git' feeds.conf.default
 
