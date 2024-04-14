fcdfb0c03aa3c3ddce3591bf0cdde21b5899575f
fay-lab-shenzhen <65667743+fay-lab-shenzhen@users.noreply.github.com>
2024-04-14 14:21:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,3 @@
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' feeds.conf.default
-sed -i '$a src-git openappfilter https://github.com/destan19/OpenAppFilter.git' feeds.conf.default
