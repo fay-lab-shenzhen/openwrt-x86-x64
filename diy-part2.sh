f4b43d480b2b41ede7426b9e840b86d4000e0f56
fay-lab-shenzhen <65667743+fay-lab-shenzhen@users.noreply.github.com>
2025-10-15 14:38:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.111.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.$((addr_offset++)).1/192.168.111.1/g' package/base-files/files/bin/config_generate
 
